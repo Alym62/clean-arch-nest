05378276d66db3284d41f58f3cc2d26b47f80c31
Alym62 <gasafm62@gmail.com>
2024-07-06 18:56:55 -0300

    refactor: env config module adjustment made.

--- src/shared/infra/env-config/env-config.module.ts
+++ src/shared/infra/env-config/env-config.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { join } from 'node:path';
 import { EnvConfigService } from './env-config.service';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [
+        join(__dirname, `../../../../.env.${process.env.NODE_ENV}`),
+      ],
+    }),
+  ],
   providers: [EnvConfigService]
 })
-export class EnvConfigModule {}
+export class EnvConfigModule { }
